9c975895b9c1d60f3f8b9ad4627dddfbee39ae4c
geekyrakshit <19soumik.rakshit96@gmail.com>
2024-09-13 11:19:35 +0000

    add: schema for structured output

---
+++ ml_frameworks_bot/keras_io/schema.py
@@ -0,0 +1,30 @@
+from typing import List, Optional
+
+from pydantic import BaseModel
+
+
+class Argument(BaseModel):
+    arg_name: str
+    description: str
+
+
+class Shape(BaseModel):
+    data_format: str
+    shape: str
+
+
+class Error(BaseModel):
+    error_name: str
+    condition: str
+
+
+class APIReference(BaseModel):
+    api_name: str
+    api_signature: str
+    description: str
+    arguments: Optional[List[Argument]] = None
+    input_shapes: Optional[List[Shape]] = None
+    output_shapes: Optional[List[Shape]] = None
+    return_type: Optional[str] = None
+    raises: Optional[Error] = None
+    example_usage: Optional[List[str]] = None
