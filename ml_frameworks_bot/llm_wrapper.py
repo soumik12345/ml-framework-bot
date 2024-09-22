6a30a6b81408724113b2d6d284acc0d2bb22a4be
geekyrakshit <19soumik.rakshit96@gmail.com>
2024-09-22 22:05:51 +0000

    add: LLMClientWrapper

---
+++ ml_frameworks_bot/llm_wrapper.py
@@ -0,0 +1,65 @@
+from typing import TYPE_CHECKING, Any, Union, get_args
+
+import anthropic
+import weave
+from instructor import Instructor
+
+if TYPE_CHECKING:
+    from anthropic import Anthropic
+    from instructor import Instructor
+    from openai import OpenAI
+
+
+OPENAI_MODELS = [
+    "gpt-4o",
+    "gpt-4o-mini",
+    "o1-preview",
+    "o1-mini",
+    "gpt-4-turbo",
+    "gpt-4",
+    "gpt-3.5-turbo",
+]
+
+
+class LLMClientWrapper(weave.Model):
+    model_name: str
+    _llm_client: Union["OpenAI", "Anthropic"] = None
+    _structured_llm_client: "Instructor" = None
+
+    def __init__(self, model_name: str):
+        super().__init__(model_name=model_name)
+        import instructor
+
+        if self.model_name in OPENAI_MODELS:
+            from openai import OpenAI
+
+            self._llm_client = OpenAI()
+            self._structured_llm_client = instructor.from_openai(self._llm_client)
+        elif self.model_name in get_args(get_args(anthropic.types.model.Model)[-1]):
+            from anthropic import Anthropic
+
+            self._llm_client = Anthropic()
+            self._structured_llm_client = instructor.from_anthropic(self._llm_client)
+        else:
+            raise ValueError(f"{self.model_name} is not supported!")
+
+    @weave.op()
+    def predict(self, **kwargs) -> Any:
+        if self.model_name in OPENAI_MODELS:
+            return (
+                self._structured_llm_client.chat.completions.create(
+                    model=self.model_name, **kwargs
+                )
+                if "response_model" in kwargs
+                else self._llm_client.chat.completions.create(
+                    model=self.model_name, **kwargs
+                )
+            )
+        elif self.model_name in get_args(get_args(anthropic.types.model.Model)[-1]):
+            return (
+                self._structured_llm_client.chat.completions.create(
+                    model=self.model_name, **kwargs
+                )
+                if "response_model" in kwargs
+                else self._llm_client.messages.create(model=self.model_name, **kwargs)
+            )
