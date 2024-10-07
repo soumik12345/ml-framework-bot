2089792d4c9f979d27b88ce9e89be45784af2a50
Saurav Maheshkar <sauravvmaheshkar@gmail.com>
2024-10-07 14:12:06 +0100

    fix: accomodate for local git repos

--- ml_frameworks_bot/utils.py
+++ ml_frameworks_bot/utils.py
@@ -22,7 +22,8 @@ def fetch_git_repository(
         repository = Repo.clone_from(repository_url, repository_local_path)
     else:
         repository = Repo(repository_local_path)
-    repository.remotes.origin.pull()
+    if len(repository.remotes) != 0:
+        repository.remotes.origin.pull()
 
 
 def get_all_file_paths(
