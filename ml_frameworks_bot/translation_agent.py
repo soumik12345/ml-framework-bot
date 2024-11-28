8133888dc161b4dfb6428af0173c0567fcaa3772
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2024-11-28 21:38:55 +0530

    update: TranslationAgent

--- ml_frameworks_bot/translation_agent.py
+++ ml_frameworks_bot/translation_agent.py
@@ -1,25 +1,30 @@
 from typing import get_args
 
+import litellm
 import weave
-
-from .llm_wrapper import LLMClientWrapper
+from pydantic import BaseModel
 from .op_extraction import DocumentationRetreiver, OpExtractor
 from .retrieval import HeuristicRetreiver
-from .utils import SUPPORTED_FRAMEWORKS
+from .utils import SUPPORTED_FRAMEWORKS, get_structured_output_from_completion
+
+
+class SupportedFrameworks(BaseModel):
+    frameworks: SUPPORTED_FRAMEWORKS
 
 
 class TranslationAgent(weave.Model):
-    model: str
+    model_name: str
+    verbose: bool = True
 
     @weave.op()
     def predict(
         self,
         code_snippet: str,
-        target_framework: str,
+        # target_framework: str,
     ):
         # identify source framework
-        framework_identification_model = FrameworkIdentificationModel(model=self.model)
-        source_framework = framework_identification_model.predict(snippet=code_snippet)
+        framework_identification_model = FrameworkIdentificationModel(model_name=self.model_name)
+        source_framework = framework_identification_model.predict(code_snippet=code_snippet)
 
         # initialise retriever
         source_retriever: DocumentationRetreiver = HeuristicRetreiver(
@@ -28,41 +33,45 @@ class TranslationAgent(weave.Model):
 
         # Op Extraction
         source_op_extractor = OpExtractor(
-            op_extraction_llm_client=LLMClientWrapper(model_name=self.model),
-            retrieval_augmentation_llm_client=LLMClientWrapper(model_name="gpt-4o"),
+            model_name=self.model_name,
             api_reference_retriever=source_retriever,
-            use_rich=False,
+            verbose=self.verbose,
         )
         source_ops = source_op_extractor.predict(code_snippet=code_snippet)  # noqa: F841
-        pass
+        return source_ops
 
 
 class FrameworkIdentificationModel(weave.Model):
-    model: str
+    model_name: str
 
     # ruff: noqa: E501
     @weave.op()
     def predict(self, code_snippet: str) -> str:
-        return LLMClientWrapper(self.model).predict(
+        completion = litellm.completion(
+            model=self.model_name,
             messages=[
                 {
-                    "role": "system",
-                    "content": f"""
-                        You are an experienced machine learning engineer expert in python,
-                        and the following frameworks: {", ".join(get_args(SUPPORTED_FRAMEWORKS))}.
-                        You will be provided with a code snippet and you need to identify from the aforementioned
-                        frameworks the code snippet belongs to. You must follow the following rules:
-                        1. If keras is imported using `from tensorflow import keras`, then the framework is keras2.
-                            Also, if tensorflow is imported using `import tensorflow as tf` and then keras is used as
-                            `tf.keras`, then the framework is keras2.
-                        2. If keras is imported using `import keras` without it being imported
-                            from tensorflow, then the framework is keras3.
-                    """,
+                "role": "system",
+                "content": f"""
+You are an experienced machine learning engineer expert in python,
+and the following frameworks: {", ".join(get_args(SUPPORTED_FRAMEWORKS))}.
+You will be provided with a code snippet and you need to identify from the aforementioned
+frameworks the code snippet belongs to. You must follow the following rules:
+1. If keras is imported using `from tensorflow import keras`, then the framework is keras2.
+Also, if tensorflow is imported using `import tensorflow as tf` and then keras is used as
+`tf.keras`, then the framework is keras2.
+2. If keras is imported using `import keras` without it being imported
+from tensorflow, then the framework is keras3.
+                """,
                 },
                 {
                     "role": "user",
                     "content": code_snippet,
                 },
             ],
-            response_model=SUPPORTED_FRAMEWORKS,
+            response_format=SupportedFrameworks,
         )
+        return get_structured_output_from_completion(
+            completion,
+            response_format=SupportedFrameworks,
+        ).frameworks
